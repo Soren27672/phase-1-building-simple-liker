dd6efa9d4fc52b32f2592c08d1b054b328ffdc6a
Maxwell Gottesman <maxgman16@yahoo.com>
2022-11-17 19:05:45 -0500

    commit tree e9e0ad7c09be511ffe0afde036145a517ce6995b

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const heartBinarium = {
+  '♡':'♥',
+  '♥':'♡'
+}
+const modal = document.getElementById('modal');
 
 // Your JavaScript code goes here!
 
+const init = () => {
+  
+  [...document.getElementsByClassName('like-glyph')].forEach(element => element.addEventListener('click', e => {
+    mimicServerCall()
+    .then(() => {
+      console.log('thru');
+      return e.target.textContent = heartBinarium[e.target.textContent]
+    })
+    .catch((oops) => {
+        modal.classList.remove('hidden');
+        document.getElementById('modal-message').textContent = oops;
+        setTimeout(() => {
+          modal.classList.add('hidden');
+        },3000);
+        
+    });
+  }));
 
+};
 
+document.addEventListener('DOMContentLoaded', init);
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
